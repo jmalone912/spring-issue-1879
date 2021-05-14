0692ffecf28e4c1ceda9e52e318ece227b138459
5007800 <joe.malone@fedex.com>
2021-05-14 17:04:46 -0400

    Initial commit

---
+++ src/main/java/com/refresh/scope/springissue1879/SpringIssue1879Application.java
@@ -0,0 +1,13 @@
+package com.refresh.scope.springissue1879;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIssue1879Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIssue1879Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/refresh/scope/springissue1879/SpringIssue1879ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refresh.scope.springissue1879;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIssue1879ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
